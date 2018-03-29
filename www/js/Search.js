36fcdeef3fa89d78a45b81aa74efd4e9913bc6a7
acmad123 <ac-mad@live.co.uk>
2018-03-29 15:53:47 +0100

    Signed-off-by: acmad123 <ac-mad@live.co.uk>

--- www/js/Search.js
+++ www/js/Search.js
@@ -23,48 +23,8 @@ function SearchClicked() {
         });
     // getting the reference to the paragraph tag on the Search.html with id = data
     var data = document.getElementById("data");
-    // chr will contain the results which will be populated on the screen.
-    var chr = ""
-
-    // searching with respect from DB here
-    myDB.transaction(function (transaction) {
-        transaction.executeSql('SELECT * FROM MyTable where property = ?', [property], function (tx, results) {
-            var len = results.rows.length;
-            // location variable will be used to store the location of the user. The latitude and langitude fields will be added to it after fetching their values from the db.
-            var location ="https://www.google.com/maps/search/?api=1&query="
-            var i;
-           
-            if (len == 0)
-            {
-                data.innerHTML = "No results to display.";
-            }
-            for (i = 0; i < len; i++) {
-          
-
-                var res = results.rows.item(i).date;
-                // spliting the date variable which contains both the time and date into two separate time and date variables.
-                    var date = res.split("T");
-                    chr += "<br /> <b> ID: <b>" + results.rows.item(i).id
-                        + "<br/><b> Property Type: <b>" + results.rows.item(i).property
-                        + "<br/><b>Number of Bedrooms: <b> " + results.rows.item(i).bedroom
-                        + "<br/><b>Name of Reporter: <b> " + results.rows.item(i).name
-                        + "<br/><b>Date: <b> " + date[0]
-                        + "<br/><b>Time: <b> " + date[1]
-                        + " <br/><b>Rent:<b> " + results.rows.item(i).rent
-                    if (results.rows.item(i).notes != "")
-                        chr += "<br/><b>Note: <b> " + results.rows.item(i).notes;
-
-                    if (results.rows.item(i).furniture != "")
-                        chr += "<br/><b> Furniture Type: <b>" + results.rows.item(i).furniture;
-                
-                // concatinating string location with the latitude and longitude. 
-                    chr += "<br/><a href=" + location + results.rows.item(i).lat + "," + results.rows.item(i).lang + ">Click to see location</a>" + "<br/><br/>************************************";
-                }
-            // data.innerHTML will populate the results on the screen.
-            if (chr != "")
-                data.innerHTML = chr;
-        }, null);
-    });
+}
+   
 
 }
 document.addEventListener('backbutton', function () {
